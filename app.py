68f1799385ada0735efbc99d9fe0abd959548262
Subhashisg <subhashisg90@gmail.com>
2025-08-31 18:04:20 +0530

    Update

--- app.py
+++ app.py
@@ -358,4 +358,6 @@ if __name__ == '__main__':
     # Create processed directory
     os.makedirs('processed', exist_ok=True)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
